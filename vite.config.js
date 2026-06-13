80480878121b7708e767a020c3f39e50aa5ca31a
Adriano Molina <adriano2008molina@gmail.com>
2026-06-13 13:06:46 -0500

    Convertir TIENDA JEROMY en PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "TIENDA JEROMY",
+        short_name: "Jeromy POS",
+
+        description:
+          "Sistema de ventas e inventario",
+
+        theme_color: "#15803d",
+
+        background_color: "#15803d",
+
+        display: "standalone",
+
+        start_url: "/",
+
+        icons: [
+          {
+            src: "/logo.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/logo.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
+});
